b957b52e7863127b2d6c3824bc425a7132c8f5db
SKYLINE2\JoachimAY <joachim.ally@skyline.be>
2023-01-18 10:54:08 +0100

    Add project files.

---
+++ AutomationScript_ClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationScript_ClassLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("AutomationScript_ClassLibrary")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E63C112A-B4C8-41BB-A67A-95277E3D3147")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ TestScript_Of_JAY_1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestScript_Of_JAY_1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestScript_Of_JAY_1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0CA7A776-37D0-44C5-A0C1-CA4CA936B0BA")]
+[assembly: AssemblyVersion("1.0.0.0")]
